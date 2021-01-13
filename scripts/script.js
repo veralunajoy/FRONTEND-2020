24263ef1666e5c3ad251989b94eab25465223404
veralunajoy <43676024+veralunajoy@users.noreply.github.com>
2021-01-13 13:40:19 +0100

    javascript, footer
    javascript (not working)

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var menuButton = document.querySelector('menu-button');
+
+menuButton.addEventListener('click', openMenu);
+
+function openMenu() {
+    var showMenu = document.querySelector('nav');
+    showMenu.classList.toggle('active');
+}
+
