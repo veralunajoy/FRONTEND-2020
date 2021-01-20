8ff53229887f157c75ef9c06402661fe2334c6b9
veralunajoy <43676024+veralunajoy@users.noreply.github.com>
2021-01-20 01:22:00 +0100

    working menu, links with animation, form
    menu is working
    links in menu have animation on hover
    form added and stylized

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
 // JavaScript Document
 
-var menuButton = document.querySelector('menu-button');
+var menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', openMenu);
+menuButton.addEventListener('click', changeIcon);
 
 function openMenu() {
     var showMenu = document.querySelector('nav');
     showMenu.classList.toggle('active');
 }
+
+function changeIcon() {
+    var changeMenuIcon = document.querySelectorAll('span');
+    changeMenuIcon.classList.toggle('cross');
+}
+
+
 
